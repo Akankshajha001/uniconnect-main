57be1c958c31049342fda4fada1490f0ef875016
kamyarathore08 <kamyarathore08@gmail.com>
2026-03-31 20:00:07 +0530

    changes made

--- services/__init__.py
+++ services/__init__.py
@@ -8,6 +8,7 @@ from .lost_found_service import (
     get_all_items,
     get_lost_items,
     get_found_items,
+    find_potential_matches,
     claim_item,
     get_item_by_id
 )
@@ -15,22 +16,20 @@ from .lost_found_service import (
 from .notes_service import (
     upload_note,
     get_notes_by_subject,
-    get_all_notes_list,
+ 
     increment_download_count,
     get_top_contributors,
     search_notes
 )
 
 from .analytics_service import (
-    get_lost_found_stats,
-    get_notes_stats,
-    get_category_distribution,
+   
     get_top_downloaded_notes
 )
 
 __all__ = [
     'add_lost_item', 'add_found_item', 'get_all_items', 'get_lost_items', 
-    'get_found_items', 'claim_item', 'get_item_by_id',
+    'get_found_items', 'find_potential_matches', 'claim_item', 'get_item_by_id',
     'upload_note', 'get_notes_by_subject', 'get_all_notes_list', 
     'increment_download_count', 'get_top_contributors', 'search_notes',
     'get_lost_found_stats', 'get_notes_stats', 'get_category_distribution',
